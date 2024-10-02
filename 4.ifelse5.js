dac27aac78a5a26c40e22d36b61a4a7474866026
Raihan Shaikh <shaikhraihan1617@gmail.com>
2024-10-02 22:47:47 +0530

    Update 4.ifelse5.js

--- 4.ifelse5.js
+++ 4.ifelse5.js
@@ -4,11 +4,11 @@ let marks = 75;
 
 if (marks > 80) {
     console.log("Student grade is A");
-} else if (marks > 60 && marks <= 80) {
+} else if (marks > 60) {
     console.log("Student grade is B");
-} else if (marks > 45 && marks <= 60) {
+} else if (marks > 45) {
     console.log("Stuent grade is C");
-} else if (marks > 30 && marks <= 45) {
+} else if (marks > 30) {
     console.log("Student grade is D");
 } else {
     console.log("Student is FAIL");
