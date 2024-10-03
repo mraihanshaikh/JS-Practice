ca8f6a61b665d7cfdd0e67f766367934e017d089
Raihan Shaikh <shaikhraihan1617@gmail.com>
2024-10-03 13:24:38 +0530

    Update 6.Loop7.js

--- 6.Loop7.js
+++ 6.Loop7.js
@@ -1,11 +1,10 @@
 // Sum of Even Numbers
 
 let num = 10;
-
-for(let i = 1; i<=num ; i++){
+let even = 0;
+for (let i = 1; i <= num; i++) {
     if (i % 2 == 0) {
-        console.log(i);
-        
-    }
-    
+        even = even + i;
+    }    
 }
+console.log(even);
