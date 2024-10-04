befc2926a128e0d44beaf180da6c8108478cb2f7
Raihan Shaikh <shaikhraihan1617@gmail.com>
2024-10-04 16:38:09 +0530

    Update 6.Loop5.js

--- 6.Loop5.js
+++ 6.Loop5.js
@@ -1,9 +1,18 @@
 // Factorial (Without a Function)
 
-let num = 3;
+let num = 5;
 let Factorial = 1;
 
 for(let i =1;i<=num;i++){
     Factorial = Factorial * i;
 }
 console.log(Factorial);
+
+// Recursion
+
+let recusion = 1;
+
+for(i = num; i>=1; i--){
+    recusion = recusion * i;
+}
+console.log(recusion);
