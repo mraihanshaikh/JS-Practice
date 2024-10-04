0ea97e73d09d2e7e108dd72117512acfcdea7feb
Raihan Shaikh <shaikhraihan1617@gmail.com>
2024-10-04 16:38:14 +0530

    Update 6.Loop7.js

--- 6.Loop7.js
+++ 6.Loop7.js
@@ -2,9 +2,11 @@
 
 let num = 10;
 let even = 0;
-for (let i = 1; i <= num; i++) {
-    if (i % 2 == 0) {
+for (let i = 0; i <= num; i=i+2) {
+    // if (i % 2 == 0) {
         even = even + i;
-    }    
+        // console.log(i);
+        
+    // }    
 }
 console.log(even);
